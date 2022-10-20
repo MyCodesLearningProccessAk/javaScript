abc6190cb60c8d8603337b13296fdcf641f893f9
keneankit01 <keneankit01@gmail.com>
2022-10-20 13:09:55 +0530

    array

---
+++ javascript/js/js6.js
@@ -0,0 +1,14 @@
+console.log("Arrays");
+
+const marks = [12,125,1215,1543];
+const fruits = ['Orange','Apple','Pineapple'];
+const mixed = ['str',89,[3,5]];
+const arr = new Array(23,123,21,'Orange');
+console.log(arr.length);
+console.log(fruits[1]);
+console.log(mixed);
+console.log(Array.isArray('dfdf'));
+arr[0] = 'Parry';
+let arrelement = arr[0];
+console.log('element :',arrelement);
+console.log(arr);

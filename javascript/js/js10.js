5f763e63049f35609171909a70431ba4ed10cae3
keneankit01 <keneankit01@gmail.com>
2022-10-21 13:54:44 +0530

    DOM Related things

---
+++ javascript/js/js10.js
@@ -0,0 +1,14 @@
+let a = window;
+// console.log(a);
+// a = prompt("Hey");
+// a = confirm("Hello");
+a = window.innerHeight;
+a = window.innerWidth;
+a = scrollX;
+a = scrollY;
+// window object is globally declared && window. is nor required to be written
+
+a = location;
+a = location.toString();
+a = window.history;
+console.log(a)
